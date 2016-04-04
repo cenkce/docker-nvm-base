6104c4baa975282ff4d884186c1467149f33176e
cenkce <mail@cenkcetinkaya.com>
2016-04-04 14:54:56 +0300

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-docker build -t nullcappone/stressmonster-nvm-base
-docker push nullcappone/stressmonster-nvm-base
+init(){
+    STATUS=$(docker-machine status)
+
+    case $STATUS in
+        Running)
+            docker-machine restart
+            ;;
+        Stopping|Saved )
+            docker-machine start
+            ;;
+        *)
+            docker-machine create
+    esac
+
+    log "docker machine env"
+    eval $(docker-machine env)
+    check
+}
+
+build(){
+    docker build -t nullcappone/stressmonster-nvm-base .
+}
+
+push(){
+    docker push nullcappone/stressmonster-nvm-base
+}
+
+$*
